fdb2db8d9fc09127f1deb539c0e1c19cd7849103
Hai Vu <haivu2004@gmail.com>
2025-04-13 11:01:04 -0700

    Complete format.py

--- src/ncopa/format.py
+++ src/ncopa/format.py
@@ -1,27 +1,49 @@
 """
 Format a nginx.conf file
 """
-
 import argparse
+import pathlib
 
 from . import directive, parse
 
 
+def create_backup(file: pathlib.Path):
+    content = file.read_bytes()
+    bak = file.with_suffix('.bak')
+    bak.write_bytes(content)
+
 def main():
     parser = argparse.ArgumentParser("nformat")
-    parser.add_argument("file")
+    parser.add_argument("file", type=pathlib.Path)
     parser.add_argument(
         "-I",
         "--indent",
         default=4,
         type=int,
-        metavar="N",
+        metavar="NUM_SPACES",
         help="number of spaces to indent",
     )
-    parser.add_argument("-i", "--inplace", help="Modify file in place")
-    parser.add_argument("-o", "--output", metavar="FILE", help="Output to file")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-i",
+        "--inplace",
+        action="store_true",
+        default=False,
+        help="Modify file in place",
+    )
+    group.add_argument("-o", "--output", type=pathlib.Path,metavar="FILE", help="Output to a file")
     options = parser.parse_args()
 
-    with open(options.file) as stream:
-        directives = parse(stream.read())
-    print(directive.to_string(directives, indent=options.indent * " "))
+    directives = parse(options.file.read_text())
+    formatted = directive.to_string(directives, indent=options.indent * " ")
+
+    if options.inplace:
+        create_backup(options.file)
+        options.file.write_text(formatted)
+    elif options.output:
+        if options.output.exists():
+            create_backup(options.output)
+        options.output.write_text(formatted)
+    else:
+        print(formatted)
+
