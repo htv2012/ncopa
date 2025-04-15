b30c0b418399af25a1e614ec6f39e11a2a9361f0
Hai Vu <haivu2004@gmail.com>
2025-04-14 19:25:00 -0700

    Format code

--- src/ncopa/version.py
+++ src/ncopa/version.py
@@ -3,7 +3,7 @@ import importlib.metadata
 
 __all__ = ["version_parser"]
 
-version = "%(prog)s " + importlib.metadata.version('ncopa')
+version = "%(prog)s " + importlib.metadata.version("ncopa")
 version_parser = argparse.ArgumentParser(add_help=False)
 version_parser.add_argument(
     "-v",
